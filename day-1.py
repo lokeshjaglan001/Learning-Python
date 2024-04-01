134c12edbf623e2051cb0adccd555f6d5b264bac
Lokesh Jaglan <165664251+lokeshjaglan001@users.noreply.github.com>
2024-04-01 18:15:12 +0530

    day - 1 of learning python

---
+++ day-1.py
@@ -0,0 +1,24 @@
+# Variables in Python
+a = 5
+b = "6"
+c = True
+d = [1,2,3,4,5]
+# 2-Dimensional Array
+e = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+f = "hello world"
+
+# Arithematic Operators
+
+a = 4 + 5 # addition operator
+b = 3 - 2 # subtraction operator
+c = 4 * 2 # multiplication operator
+d = 9 / 3 # division operator
+e = 5 // 2 # quotient 
+f = 2 ** 3 # power operator
+g = 9 % 2 # remainder or modulus operator
+
