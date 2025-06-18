440943c2f8faf1385669ddc7eba0eb34451ac805
Lokesh Jaglan <lokeshjaglan01@gmail.com>
2025-06-18 15:45:19 +0530

    done

---
+++ nDay-8.py
@@ -0,0 +1,36 @@
+#Variables
+
+Variable = "Hello this is my basic variable and to make any variable we have to write a name and then give equals to sign (=) next and then you can give value according to you." 
+print(Variable)
+
+Name = input("Enter your Name : ")
+print(f"Hello {Name}") # Using input data in any print function
+
+# Data Types
+
+var1 = "Hello Sir !" # This is String data type value
+print(f"Data type of {var1} is " ,type(var1))
+
+var2 = 244 # This is Integer data type value
+print(f"Data type of {var2} is " ,type(var2))
+
+var3 = True # This is Boolean data type value
+print(f"Data type of {var3} is " ,type(var3))
+
+var4 = 7.99 # This is Float data type value
+print(f"Data type of {var4} is " ,type(var4))
+
+#Operators
+
+Num1 = 345
+Num2 = 667
+Num3 = 773
+Num4 = 243
+
+print(f"Addition of {Num1} and {Num2} is ",Num1 + Num2) # Plus operator which do Addition
+
+print(f"Substraction of {Num2} and {Num1} is ",Num2 - Num1) # Minus operator which do Addition
+
+print(f"Multiplication of {Num3} and {Num4} is " , Num3*Num4) # Multiply operator which do Multiplication
+
+print(f"Division of {Num3} with {Num4} is " , Num3/Num4) # Dividing operator which do Division
